a5f249044d21cdf9ccbcf2d137fa580a5fb56c80
Piotr Kulpinski <piotr@kulpinski.pl>
2025-01-15 10:08:57 +0100

    update tsconfig and npm packages

--- build.ts
+++ build.ts
@@ -1,16 +1,19 @@
 /// <reference types="bun-types" />
 
-const { outputs } = await Bun.build({
-  entrypoints: ["./src/index.ts"],
-  format: "esm",
-  outdir: "./dist",
-  external: ["react", "react-dom"],
-})
+async function build() {
+  const { outputs } = await Bun.build({
+    entrypoints: ["./src/index.ts"],
+    format: "esm",
+    outdir: "./dist",
+    external: ["react", "react-dom"],
+  })
 
-for (const output of outputs) {
-  const bytes = await output.arrayBuffer()
-  const prefix = '"use client"\n\n'
-  await Bun.write(output.path, [prefix, bytes])
+  for (const output of outputs) {
+    const bytes = await output.arrayBuffer()
+    const prefix = '"use client"\n\n'
+    await Bun.write(output.path, [prefix, bytes])
+  }
 }
 
+build()
 export type {}
